d6c8d00a94658b99aaf037d56ffc20ccf729dc0a
Justin Frank <justin@pop-os.localdomain>
2022-03-13 14:25:12 -0700

    added brew function

--- coffee.py
+++ coffee.py
@@ -1,7 +1,7 @@
 import ingredients
 import aeropress
 
-def main():
+def brew():
     myAeropress = aeropress.aeropress(filter = 'paper')
     myWater = ingredients.water(temp = 20)
     myBeans = ingredients.beans(origin = 'Kenya', roast = 'light', grind = 0);
@@ -9,6 +9,9 @@ def main():
     hotWater = ingredients.heatWater(myWater,temp=100)
     myCoffee = aeropress.press(inWater = hotWater, inBeans = groundBeans, inAeropress = myAeropress)
     myCoffee.coffeePrint()
+
+def main():
+    brew()
 
 if __name__ == '__main__':
     main()
