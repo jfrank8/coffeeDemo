8b577e49308c6be24ddbeffb174c3c7c2eec59ad
Justin Frank <justin@pop-os.localdomain>
2022-03-15 16:27:56 -0700

    updated coffee.py

--- coffee.py
+++ coffee.py
@@ -2,12 +2,12 @@ import coffeeDemo.ingredients
 import coffeeDemo.aeropress
 
 def brew():
-    myAeropress = aeropress.aeropress(filter = 'paper')
-    myWater = ingredients.water(temp = 20)
-    myBeans = ingredients.beans(origin = 'Kenya', roast = 'light', grind = 0);
-    groundBeans = ingredients.grind(myBeans, grind='fine')
-    hotWater = ingredients.heatWater(myWater,temp=100)
-    myCoffee = aeropress.press(inWater = hotWater, inBeans = groundBeans, inAeropress = myAeropress)
+    myAeropress = coffeeDemo.aeropress.aeropress(filter = 'paper')
+    myWater = coffeeDemo.ingredients.water(temp = 20)
+    myBeans = coffeeDemo.ingredients.beans(origin = 'Kenya', roast = 'light', grind = 0);
+    groundBeans = coffeeDemo.ingredients.grind(myBeans, grind='fine')
+    hotWater = coffeeDemo.ingredients.heatWater(myWater,temp=100)
+    myCoffee = coffeeDemo.aeropress.press(inWater = hotWater, inBeans = groundBeans, inAeropress = myAeropress)
     myCoffee.coffeePrint()
 
 def main():
